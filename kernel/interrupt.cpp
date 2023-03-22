bb89aa72dfdeb8eaf66e2e0faeab850621fc3aed
junya uchizono <junya19991215@icloud.com>
2023-03-22 12:13:12 +0900

    impl intterupt

---
+++ kernel/interrupt.cpp
@@ -0,0 +1,42 @@
+#include "interrupt.hpp"
+
+std::array<IDTEntry, kIDTEntryNum> idt;
+
+void registerIDTEntry(int vector, IDTEntryTypeAttribute attr, uint64_t offset,
+                      uint16_t segment_selector) {
+    idt[vector].attr = attr;
+    idt[vector].segment_selector = segment_selector;
+    idt[vector].offset_low = offset & 0xffffu;
+    idt[vector].offset_mid = (offset >> 16) & 0xffffu;
+    idt[vector].offset_hi = offset >> 32;
+}
+
+void notifyEOI() {
+    volatile auto eoi = reinterpret_cast<uint32_t*>(0xfee000b0);
+    *eoi = 0;
+}
+
+void loadIDT() {
+    uint16_t size = sizeof(idt) - 1;
+    uint64_t address = reinterpret_cast<uintptr_t>(&idt[0]);
+
+    struct {
+        uint16_t limit;
+        uint64_t base;
+    } __attribute__((packed)) idtr = {size, address};
+    __asm__("lidt %0" : : "m"(idtr));
+}
+
+namespace {
+__attribute__((interrupt)) void InterruptHandlerLocalAPICTimer(
+    InterruptFrame* frame) {}
+}  // namespace
+
+void initializeInterruptConfig() {
+    const uint16_t kKernelCS = 1 << 3;
+    registerIDTEntry(
+        kIDTEntryNum, MakeIDTAttr(DescriptorType::kInterruptGate, 0),
+        reinterpret_cast<uint64_t>(InterruptHandlerLocalAPICTimer), kKernelCS);
+
+    loadIDT();
+}
--- kernel/interrupt.hpp
+++ kernel/interrupt.hpp
@@ -15,11 +15,11 @@ enum class DescriptorType {
     kExecuteRead = 10,
 };
 
-union InterruptDescriptorAttribute {
+union IDTEntryTypeAttribute {
     uint16_t data;
 
     struct {
-        uint16_t interrupt_stack_table : 3;
+        uint16_t ist : 3;
         uint16_t : 5;
         DescriptorType type : 4;
         uint16_t : 1;
@@ -27,3 +27,54 @@ union InterruptDescriptorAttribute {
         uint16_t present : 1;
     } __attribute__((packed)) bits;
 } __attribute__((packed));
+
+struct IDTEntry {
+    uint16_t offset_low;
+    uint16_t segment_selector;
+    IDTEntryTypeAttribute attr;
+    uint16_t offset_mid;
+    uint32_t offset_hi;
+    uint32_t reserved;
+} __attribute__((packed));
+
+const int kIDTEntryNum = 256;
+
+extern std::array<IDTEntry, kIDTEntryNum> idt;
+
+constexpr IDTEntryTypeAttribute MakeIDTAttr(DescriptorType type,
+                                            uint8_t privilege_level,
+                                            bool present = true,
+                                            uint8_t ist = 0) {
+    IDTEntryTypeAttribute attr{};
+    attr.bits.ist = ist;
+    attr.bits.type = type;
+    attr.bits.descriptor_privilege_level = privilege_level;
+    attr.bits.present = present;
+
+    return attr;
+};
+
+void registerIDTEntry(InterruptVector::Number vector,
+                      IDTEntryTypeAttribute attr, uint64_t offset,
+                      uint16_t segment_selector);
+
+class InterruptVector {
+   public:
+    enum Number {
+        kLocalAPICTimer = 0x40,
+    };
+};
+
+struct InterruptFrame {
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t rflags;
+    uint64_t rsp;
+    uint64_t ss;
+};
+
+void notifyEOI();
+
+void initializeInterruptConfig();
+
+void LoadIDT();
