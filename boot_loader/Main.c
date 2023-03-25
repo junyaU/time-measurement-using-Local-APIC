11c2cbb26e373eedbb2a64ed2415ff654b9d752f
junya uchizono <junya19991215@icloud.com>
2023-03-25 17:13:30 +0900

    refactor boot_loader

--- boot_loader/Main.c
+++ boot_loader/Main.c
@@ -14,18 +14,14 @@
 #include "frame_buffer_config.hpp"
 #include "memory_map.hpp"
 
-void Halt()
-{
-    while (1)
-    {
+void Halt() {
+    while (1) {
         __asm__("hlt");
     }
 }
 
-EFI_STATUS GetMemoryMap(struct MemoryMap *memory_map)
-{
-    if (memory_map->buffer == NULL)
-    {
+EFI_STATUS GetMemoryMap(struct MemoryMap *memory_map) {
+    if (memory_map->buffer == NULL) {
         return EFI_BUFFER_TOO_SMALL;
     }
 
@@ -37,8 +33,7 @@ EFI_STATUS GetMemoryMap(struct MemoryMap *memory_map)
                              &memory_map->descriptor_version);
 }
 
-EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root)
-{
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
     EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
     EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
 
@@ -56,16 +51,13 @@ EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root)
 }
 
 void CalcLoadAddressRange(Elf64_Ehdr *ehdr, UINT64 *first_addr,
-                          UINT64 *last_addr)
-{
+                          UINT64 *last_addr) {
     Elf64_Phdr *phdr = (Elf64_Phdr *)((UINT64)ehdr + ehdr->e_phoff);
     *first_addr = MAX_UINT64;
     *last_addr = 0;
 
-    for (Elf64_Half i = 0; i < ehdr->e_phnum; i++)
-    {
-        if (phdr[i].p_type != PT_LOAD)
-        {
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; i++) {
+        if (phdr[i].p_type != PT_LOAD) {
             continue;
         }
 
@@ -74,13 +66,10 @@ void CalcLoadAddressRange(Elf64_Ehdr *ehdr, UINT64 *first_addr,
     }
 }
 
-void CopyLoadSegments(Elf64_Ehdr *ehdr)
-{
+void CopyLoadSegments(Elf64_Ehdr *ehdr) {
     Elf64_Phdr *phdr = (Elf64_Phdr *)((UINT64)ehdr + ehdr->e_phoff);
-    for (Elf64_Half i = 0; i < ehdr->e_phnum; i++)
-    {
-        if (phdr[i].p_type != PT_LOAD)
-        {
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; i++) {
+        if (phdr[i].p_type != PT_LOAD) {
             continue;
         }
 
@@ -96,8 +85,7 @@ void CopyLoadSegments(Elf64_Ehdr *ehdr)
 
 // Accessing Graphics Board Functions
 EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
-                   EFI_GRAPHICS_OUTPUT_PROTOCOL **gop)
-{
+                   EFI_GRAPHICS_OUTPUT_PROTOCOL **gop) {
     EFI_STATUS Status;
     UINTN num_gop_handles = 0;
     EFI_HANDLE *gop_handles = NULL;
@@ -105,16 +93,14 @@ EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
     Status =
         gBS->LocateHandleBuffer(ByProtocol, &gEfiGraphicsOutputProtocolGuid,
                                 NULL, &num_gop_handles, &gop_handles);
-    if (EFI_ERROR(Status))
-    {
+    if (EFI_ERROR(Status)) {
         return Status;
     }
 
     Status = gBS->OpenProtocol(gop_handles[0], &gEfiGraphicsOutputProtocolGuid,
                                (VOID **)gop, image_handle, NULL,
                                EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-    if (EFI_ERROR(Status))
-    {
+    if (EFI_ERROR(Status)) {
         return Status;
     }
 
@@ -123,129 +109,154 @@ EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
     return EFI_SUCCESS;
 }
 
-EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
-                           EFI_SYSTEM_TABLE *system_table)
-{
-    Print(L"hello!");
+EFI_STATUS GetFrameBufferConfig(EFI_HANDLE image_handle,
+                                struct FrameBufferConfig *config) {
+    EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+    EFI_STATUS status = OpenGOP(image_handle, &gop);
+    if (EFI_ERROR(status)) {
+        Print(L"failed to open GOP\n");
+        return status;
+    }
 
+    config->frame_buffer = (UINT8 *)gop->Mode->FrameBufferBase;
+    config->pixels_per_scan_line = gop->Mode->Info->PixelsPerScanLine;
+    config->horizon_resolution = gop->Mode->Info->HorizontalResolution;
+    config->vertical_resolution = gop->Mode->Info->VerticalResolution;
+
+    switch (gop->Mode->Info->PixelFormat) {
+        case PixelRedGreenBlueReserved8BitPerColor:
+            config->pixel_format = kPixelRGB8BitPerColor;
+            break;
+        case PixelBlueGreenRedReserved8BitPerColor:
+            config->pixel_format = kPixelBGR8BitPerColor;
+            break;
+        default:
+            Print(L"Unimplemented pixel format: %d\n",
+                  gop->Mode->Info->PixelFormat);
+            return EFI_UNSUPPORTED;
+    }
+
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS LoadKernel(EFI_HANDLE image_handle, UINT64 *kernel_buffer) {
     EFI_FILE_PROTOCOL *root_dir;
-    if (EFI_ERROR(OpenRootDir(image_handle, &root_dir)))
-    {
+    EFI_STATUS status;
+
+    status = OpenRootDir(image_handle, &root_dir);
+    if (EFI_ERROR(status)) {
         Print(L"failed to open root directory\n");
-        Halt();
+        return status;
     }
 
     EFI_FILE_PROTOCOL *kernel;
-    if (EFI_ERROR(root_dir->Open(root_dir, &kernel, L"\\kernel.elf",
-                                 EFI_FILE_MODE_READ, 0)))
-    {
+    status = root_dir->Open(root_dir, &kernel, L"\\kernel.elf",
+                            EFI_FILE_MODE_READ, 0);
+    if (EFI_ERROR(status)) {
         Print(L"failed to open kernel\n");
-        Halt();
+        return status;
     }
 
     UINTN kernel_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
     UINT8 kernel_info_buffer[kernel_info_size];
-
-    if (EFI_ERROR(kernel->GetInfo(kernel, &gEfiFileInfoGuid, &kernel_info_size,
-                                  kernel_info_buffer)))
-    {
+    status = kernel->GetInfo(kernel, &gEfiFileInfoGuid, &kernel_info_size,
+                             kernel_info_buffer);
+    if (EFI_ERROR(status)) {
         Print(L"failed to get kernel info\n");
-        Halt();
+        return status;
     }
 
     EFI_FILE_INFO *kernel_info = (EFI_FILE_INFO *)kernel_info_buffer;
     UINTN kernel_file_size = kernel_info->FileSize;
-
     VOID *kernel_buf;
-    if (EFI_ERROR(
-            gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buf)))
-    {
+    status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buf);
+    if (EFI_ERROR(status)) {
         Print(L"failed to allocate pool\n");
-        Halt();
+        return status;
     }
 
-    if (EFI_ERROR(kernel->Read(kernel, &kernel_file_size, kernel_buf)))
-    {
+    status = kernel->Read(kernel, &kernel_file_size, kernel_buf);
+    if (EFI_ERROR(status)) {
         Print(L"error\n");
-        Halt();
+        return status;
     }
 
     Elf64_Ehdr *kernel_ehdr = (Elf64_Ehdr *)kernel_buf;
-    UINT64 kernel_start_addr, kernel_end_addr;
-    CalcLoadAddressRange(kernel_ehdr, &kernel_start_addr, &kernel_end_addr);
+    UINT64 kernel_end_addr;
+    CalcLoadAddressRange(kernel_ehdr, kernel_buffer, &kernel_end_addr);
 
-    UINTN num_pages = (kernel_end_addr - kernel_start_addr + 0xfff) / 0x1000;
-    if (EFI_ERROR(gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages,
-                                     &kernel_start_addr)))
-    {
+    UINTN num_pages = (kernel_end_addr - *kernel_buffer + 0xfff) / 0x1000;
+    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages,
+                                kernel_buffer);
+    if (EFI_ERROR(status)) {
         Print(L"failed to allocate pages\n");
-        Halt();
+        return status;
     }
     CopyLoadSegments(kernel_ehdr);
 
-    if (EFI_ERROR(gBS->FreePool(kernel_buf)))
-    {
+    status = gBS->FreePool(kernel_buf);
+    if (EFI_ERROR(status)) {
         Print(L"failed to free pool\n");
+        return status;
+    }
+
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS GetRSDP(EFI_SYSTEM_TABLE *system_table, VOID **rsdp) {
+    for (UINTN i = 0; i < system_table->NumberOfTableEntries; ++i) {
+        if (CompareGuid(&gEfiAcpiTableGuid,
+                        &system_table->ConfigurationTable[i].VendorGuid)) {
+            *rsdp = system_table->ConfigurationTable[i].VendorTable;
+            break;
+        }
+    }
+
+    if (*rsdp == NULL) {
+        Print(L"could not retrieve RSDP\n");
+        return EFI_UNSUPPORTED;
+    }
+
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS EFIAPI uefi_main(EFI_HANDLE image_handle,
+                            EFI_SYSTEM_TABLE *system_table) {
+    UINT64 kernel_buffer;
+    if (EFI_ERROR(LoadKernel(image_handle, &kernel_buffer))) {
         Halt();
     }
 
-    Print(L"nice");
-
-    EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
-    if (EFI_ERROR(OpenGOP(image_handle, &gop)))
-    {
-        Print(L"failed to open GOP\n");
+    struct FrameBufferConfig frame_buffer_config = {NULL, 0, 0, 0, 0};
+    if (EFI_ERROR(GetFrameBufferConfig(image_handle, &frame_buffer_config))) {
         Halt();
     }
 
     CHAR8 memory_map_buffer[4096 * 4];
     struct MemoryMap memory_map = {
         sizeof(memory_map_buffer), memory_map_buffer, 0, 0, 0, 0};
-    if (EFI_ERROR(GetMemoryMap(&memory_map)))
-    {
+    if (EFI_ERROR(GetMemoryMap(&memory_map))) {
         Print(L"failed to get memory map\n");
         Halt();
     }
 
-    if (EFI_ERROR(gBS->ExitBootServices(image_handle, memory_map.map_key)))
-    {
+    if (EFI_ERROR(gBS->ExitBootServices(image_handle, memory_map.map_key))) {
         Print(L"could not exit boot service\n");
         Halt();
     }
 
-    struct FrameBufferConfig config = {(UINT8 *)gop->Mode->FrameBufferBase, gop->Mode->Info->PixelsPerScanLine,
-                                       gop->Mode->Info->HorizontalResolution,
-                                       gop->Mode->Info->VerticalResolution, 0};
-
-    switch (gop->Mode->Info->PixelFormat)
-    {
-    case PixelRedGreenBlueReserved8BitPerColor:
-        config.pixel_format = kPixelRGB8BitPerColor;
-        break;
-    case PixelBlueGreenRedReserved8BitPerColor:
-        config.pixel_format = kPixelBGR8BitPerColor;
-        break;
-    default:
-        Print(L"Unimplemented pixel format: %d\n",
-              gop->Mode->Info->PixelFormat);
+    VOID *rsdp = NULL;
+    if (EFI_ERROR(GetRSDP(system_table, &rsdp))) {
         Halt();
     }
 
-    VOID *rsdp = NULL;
-    for (UINTN i = 0; i < system_table->NumberOfTableEntries; ++i)
-    {
-        if (CompareGuid(&gEfiAcpiTableGuid, &system_table->ConfigurationTable[i].VendorGuid))
-        {
-            rsdp = system_table->ConfigurationTable[i].VendorTable;
-            break;
-        }
-    }
-
     EFI_PHYSICAL_ADDRESS kernel_entry_offset = 24;
-    UINT64 kernel_entry_addr = *(UINT64 *)(kernel_start_addr + kernel_entry_offset);
+    UINT64 kernel_entry_addr = *(UINT64 *)(kernel_buffer + kernel_entry_offset);
 
-    typedef void __attribute__((sysv_abi)) KernelEntry(const struct FrameBufferConfig *, const struct MemoryMap *, const VOID *);
-    ((KernelEntry *)kernel_entry_addr)(&config, &memory_map, rsdp);
+    typedef void __attribute__((sysv_abi))
+    KernelEntry(const struct FrameBufferConfig *, const struct MemoryMap *,
+                const VOID *);
+    ((KernelEntry *)kernel_entry_addr)(&frame_buffer_config, &memory_map, rsdp);
 
     while (1)
         ;
